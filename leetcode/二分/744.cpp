c462e39878e65163e2e12e06312a64cf79058e33
bozzlau <jiaoyanglele8@gmail.com>
2025-06-10 19:21:20 +0800

    add template

---
+++ leetcode/二分/744.cpp
@@ -0,0 +1,26 @@
+/*
+给你一个字符数组 letters，该数组按非递减顺序排序，以及一个字符 target。letters 里至少有两个不同的字符。
+返回 letters 中大于 target 的最小的字符。如果不存在这样的字符，则返回 letters 的第一个字符。
+示例 1：
+输入: letters = ["c", "f", "j"]，target = "a"
+输出: "c"
+解释：letters 中字典上比 'a' 大的最小字符是 'c'。
+*/
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        return 'a';
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<char> letters = {'c', 'f', 'j'};
+    char target = 'a';
+    printf("%c\n", sol.nextGreatestLetter(letters, target));
+    return 0;
+}
